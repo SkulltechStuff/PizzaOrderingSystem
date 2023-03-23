7a1486529fd6e8d2d08e63616bbc0181517e673f
SkulltechStuff <rt652@student.aru.ac.uk>
2023-03-23 15:21:05 +0000

    Completed Minimum Viable Product ready for testing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ class Program
     static string pizzaSize = "";
     static int pizCrustLoopCheck = 1;
     static string crustType = "";
+    static int pizSauceLoopCheck = 1;
+    static string pizzaSauce = "";
+    static int pizTopLoopCheck = 1;
+    static int continueLoopCheck = 1;
+    static List<string> pizzaToppings = new List<string>();
     static void Main(string[] args)
     {
         Console.WriteLine("Its Pizza Time! Press any key to continue");
@@ -14,6 +19,9 @@ class Program
         PizzaType();
         PizzaSize();
         CrustType();
+        PizzaSauce();
+        PizzaToppings();
+        PizzaDetails();
         Console.WriteLine("Press any key to finish");
         Console.ReadKey(true);
     }
@@ -107,12 +115,182 @@ class Program
         string pizzaSauceInput = "";
         if(pizzaType == "savoury")
         {
-
+            while(pizSauceLoopCheck == 1)
+            {
+                Console.WriteLine("Please select your sauce type: Tomato, BBQ, or Ranch?");
+                pizzaSauceInput = Console.ReadLine();
+                if(pizzaSauceInput == "Tomato" || pizzaSauceInput == "tomato" || pizzaSauceInput == "TOMATO")
+                {
+                    pizzaSauce = "tomato";
+                    pizSauceLoopCheck = 0;
+                }
+                else if(pizzaSauceInput == "Bbq" || pizzaSauceInput == "bbq" || pizzaSauceInput == "BBQ")
+                {
+                    pizzaSauce = "bbq";
+                    pizSauceLoopCheck = 0;
+                }
+                else if(pizzaSauceInput == "Ranch" || pizzaSauceInput == "ranch" || pizzaSauceInput == "RANCH")
+                {
+                    pizzaSauce = "ranch";
+                    pizSauceLoopCheck = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Error, invalid input");
+                } 
+            }
         }
         else
         {
-            
+            while(pizSauceLoopCheck == 1)
+            {
+                Console.WriteLine("Please select your chocolate sauce type: White, Milk, or Dark?");
+                pizzaSauceInput = Console.ReadLine();
+                if(pizzaSauceInput == "White" || pizzaSauceInput == "white" || pizzaSauceInput == "WHITE")
+                {
+                    pizzaSauce = "white choc";
+                    pizSauceLoopCheck = 0;
+                }
+                else if(pizzaSauceInput == "Milk" || pizzaSauceInput == "milk" || pizzaSauceInput == "MILK")
+                {
+                    pizzaSauce = "milk choc";
+                    pizSauceLoopCheck = 0;
+                }
+                else if(pizzaSauceInput == "Dark" || pizzaSauceInput == "dark" || pizzaSauceInput == "DARK")
+                {
+                    pizzaSauce = "dark choc";
+                    pizSauceLoopCheck = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Error, invalid input");
+                } 
+            }
         }
     }
-
+    static void PizzaToppings()
+    {
+        string pizTopInput = "";
+        string continueProgram = "";
+        while(pizTopLoopCheck == 1)
+        {
+            continueLoopCheck = 1;
+            if(pizzaType == "savoury")
+            {
+                Console.WriteLine("Please choose from the available toppings: Anchovies, Bell Peppers, Chicken, Ham, Jalapenos, Meatballs, Mozzarella, Mushrooms, Pepperoni, Pineapple, Sweetcorn");
+                pizTopInput = Console.ReadLine();
+                if(pizTopInput == "Anchovies" || pizTopInput == "anchovies" || pizTopInput == "ANCHOVIES")
+                {
+                    pizzaToppings.Add("Anchovies");
+                }
+                else if(pizTopInput == "Bell Peppers" || pizTopInput == "bell peppers" || pizTopInput == "Bell Peppers")
+                {
+                    pizzaToppings.Add("Bell Peppers");
+                }
+                else if(pizTopInput == "Chicken" || pizTopInput == "chicken" || pizTopInput == "CHICKEN")
+                {
+                    pizzaToppings.Add("Chicken");
+                }
+                else if(pizTopInput == "Ham" || pizTopInput == "ham" || pizTopInput == "HAM")
+                {
+                    pizzaToppings.Add("Ham");
+                }
+                else if(pizTopInput == "Jalapenos" || pizTopInput == "jalapenos" || pizTopInput == "JALAPENOS")
+                {
+                    pizzaToppings.Add("Jalopenos");
+                }
+                else if(pizTopInput == "Meatballs" || pizTopInput == "meatballs" || pizTopInput == "MEATBALLS")
+                {
+                    pizzaToppings.Add("Meatballs");
+                }
+                else if(pizTopInput == "Mozzarella" || pizTopInput == "mozzarella" || pizTopInput == "MOZZARELLA")
+                {
+                    pizzaToppings.Add("Mozzarella");
+                }
+                else if(pizTopInput == "Mushrooms" || pizTopInput == "mushrooms" || pizTopInput == "MUSHROOMS")
+                {
+                    pizzaToppings.Add("Mushrooms");
+                }
+                else if(pizTopInput == "Pepperoni" || pizTopInput == "pepperoni" || pizTopInput == "PEPPERONI")
+                {
+                    pizzaToppings.Add("Pepperoni");
+                }
+                else if(pizTopInput == "Pineapple" || pizTopInput == "pineapple" || pizTopInput == "PINEAPPLE")
+                {
+                    pizzaToppings.Add("Pineapple");
+                }
+                else if(pizTopInput == "Sweetcorn" || pizTopInput == "sweetcorn" || pizTopInput == "SWEETCORN")
+                {
+                    pizzaToppings.Add("Sweetcorn");
+                }
+                else
+                {
+                    Console.WriteLine("Error, invalid input");
+                } 
+            }
+            else
+            {
+                Console.WriteLine("Please choose from the available toppings: Candy Corn, M&Ms, Marshmallow, Nuts, Oreo, Skittles, Strawberries");
+                pizTopInput = Console.ReadLine();
+                if(pizTopInput == "Candy Corn" || pizTopInput == "candy corn" || pizTopInput == "CANDY CORN")
+                {
+                    pizzaToppings.Add("Candy Corn");
+                }
+                else if(pizTopInput == "M&Ms" || pizTopInput == "m&ms" || pizTopInput == "M&MS")
+                {
+                    pizzaToppings.Add("M&Ms");
+                }
+                else if(pizTopInput == "Marshmallow" || pizTopInput == "marshmallow" || pizTopInput == "MARSHMALLOW")
+                {
+                    pizzaToppings.Add("Marshmallow");
+                }
+                else if(pizTopInput == "Nuts" || pizTopInput == "nuts" || pizTopInput == "NUTS")
+                {
+                    pizzaToppings.Add("Nuts");
+                }
+                else if(pizTopInput == "Oreo" || pizTopInput == "oreo" || pizTopInput == "OREO")
+                {
+                    pizzaToppings.Add("Oreo");
+                }
+                else if(pizTopInput == "Skittles" || pizTopInput == "skittles" || pizTopInput == "SKITTLES")
+                {
+                    pizzaToppings.Add("Skittles");
+                }
+                else if(pizTopInput == "Strawberries" || pizTopInput == "strawberries" || pizTopInput == "STRAWBERRIES")
+                {
+                    pizzaToppings.Add("Strawberries");
+                }
+                else
+                {
+                    Console.WriteLine("Error, invalid input");
+                }
+            }
+            while(continueLoopCheck == 1)
+            {
+                Console.WriteLine("Do you want any more toppings?");
+                continueProgram = Console.ReadLine();
+                if(continueProgram == "no" || continueProgram == "n")
+                {
+                    pizTopLoopCheck = 0;
+                    continueLoopCheck = 0;
+                }
+                else if (continueProgram == "yes" || continueProgram == "y")
+                {
+                    continueLoopCheck = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Error, invalid input");
+                }
+            }
+        }
+    }
+    static void PizzaDetails()
+    {
+        Console.WriteLine($"Pizza Type: {pizzaType}");
+        Console.WriteLine($"Pizza Size: {pizzaSize}");
+        Console.WriteLine($"Crust Type: {crustType}");
+        Console.WriteLine($"Pizza Sauce: {pizzaSauce}");
+        Console.WriteLine($"Pizza Toppings: {pizzaToppings}");
+    }
 }
