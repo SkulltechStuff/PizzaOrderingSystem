459b13a82bcfe5a6a18ba485d925c84abfec94b1
SkulltechStuff <rt652@student.aru.ac.uk>
2023-03-23 11:30:33 +0000

    Added PizzaSize

--- Program.cs
+++ Program.cs
@@ -3,11 +3,15 @@ class Program
 {
     static int pizTypeLoopCheck = 1;
     static string pizzaType = "";
+
+    static int pizSizeLoopCheck = 1;
+    static string pizzaSize = "";
     static void Main(string[] args)
     {
         Console.WriteLine("Its Pizza Time! Press any key to continue");
         Console.ReadKey(true);
         PizzaType();
+        PizzaSize();
         Console.WriteLine("Press any key to finish");
         Console.ReadKey(true);
     }
@@ -22,13 +26,11 @@ class Program
             if(pizzaTypeInput == "Savoury" || pizzaTypeInput == "savoury" || pizzaTypeInput == "SAVOURY")
             {
                 pizzaType = "savoury";
-                Console.WriteLine(pizzaType);
                 pizTypeLoopCheck = 0;
             }
             else if(pizzaTypeInput == "Sweet" || pizzaTypeInput == "sweet" || pizzaTypeInput == "SWEET")
             {
                 pizzaType = "sweet";
-                Console.WriteLine(pizzaType);
                 pizTypeLoopCheck = 0;
             }
             else
@@ -37,4 +39,33 @@ class Program
             }
         }
     }
+    static void PizzaSize()
+    {
+        string pizzaSizeInput = "";
+        while(pizSizeLoopCheck == 1)
+        {
+            Console.WriteLine("Please select your pizza size: Small, Medium or Large?");
+            pizzaSizeInput = Console.ReadLine();
+            if(pizzaSizeInput == "Small" || pizzaSizeInput == "small" || pizzaSizeInput == "SMALL")
+            {
+                pizzaSize = "small";
+                pizSizeLoopCheck = 0;
+            }
+            else if(pizzaSizeInput == "Medium" || pizzaSizeInput == "medium" || pizzaSizeInput == "MEDIUM")
+            {
+                pizzaSize = "medium";
+                pizSizeLoopCheck = 0;
+            }
+            else if(pizzaSizeInput == "Large" || pizzaSizeInput == "large" || pizzaSizeInput == "LARGE")
+            {
+                pizzaSize = "large";
+                pizSizeLoopCheck = 0;
+            }
+            else
+            {
+                Console.WriteLine("Error, invalid input");
+            }
+        }
+    }
+
 }
